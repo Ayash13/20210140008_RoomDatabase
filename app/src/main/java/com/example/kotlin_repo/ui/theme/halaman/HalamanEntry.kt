0055a6fae1d5c610771b86e4a2ba74a5e5be6747
Ayash13 <m.ayashal.f@gmail.com>
2023-12-04 22:14:37 +0700

    create function EntrySiswaBody

--- app/src/main/java/com/example/kotlin_repo/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/kotlin_repo/ui/theme/halaman/HalamanEntry.kt
@@ -1,5 +1,11 @@
 package com.example.kotlin_repo.ui.theme.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -7,12 +13,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.kotlin_repo.R
+import com.example.kotlin_repo.model.DetailSiswa
 import com.example.kotlin_repo.model.EntryViewModel
 import com.example.kotlin_repo.model.PenyediaViewModel
+import com.example.kotlin_repo.model.UIStateSiswa
 import com.example.kotlin_repo.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.launch
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
@@ -26,7 +36,20 @@ fun EntrySiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
-    val coroutineScope = rememberCoroutineScope()
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+}
+
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa: UIStateSiswa,
+    onSiswaValueChange: (DetailSiswa) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_Large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+
+    }
 }
