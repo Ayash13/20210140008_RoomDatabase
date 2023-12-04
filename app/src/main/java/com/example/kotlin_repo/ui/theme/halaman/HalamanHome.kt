dec160827ec462d5ff130ef93cc5947a04dd8698
Ayash13 <m.ayashal.f@gmail.com>
2023-12-04 22:50:39 +0700

    Create ListSiswa with LazyCoumn inside it, then call DataSiswa

--- app/src/main/java/com/example/kotlin_repo/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/kotlin_repo/ui/theme/halaman/HalamanHome.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Phone
@@ -37,6 +39,23 @@ import com.example.kotlin_repo.navigasi.DestinasiNavigasi
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
+}
+
+@Composable
+fun ListSiswa(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = Modifier) {
+        items(items = itemSiswa, key = { it.id }) {
+                person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_smalL))
+            )
+        }
+    }
 }
 
 @Composable
