e1049c54ff7719d0460149add10adf13555e5a40
Ayash13 <m.ayashal.f@gmail.com>
2023-12-04 21:02:53 +0700

    create class HomeViewModel and add parameter also extend the class

---
+++ app/src/main/java/com/example/kotlin_repo/model/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.kotlin_repo.model
+
+import androidx.lifecycle.ViewModel
+import com.example.kotlin_repo.repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+}
