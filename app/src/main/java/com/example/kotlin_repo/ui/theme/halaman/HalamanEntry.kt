89f39fc9246c8cbd6e29db1137c6f4b0f30526ee
Ayash13 <m.ayashal.f@gmail.com>
2023-12-04 22:07:11 +0700

    create object DestinasiEntry

--- app/src/main/java/com/example/kotlin_repo/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/kotlin_repo/ui/theme/halaman/HalamanEntry.kt
@@ -3,3 +3,7 @@ package com.example.kotlin_repo.ui.theme.halaman
 import com.example.kotlin_repo.R
 import com.example.kotlin_repo.navigasi.DestinasiNavigasi
 
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
