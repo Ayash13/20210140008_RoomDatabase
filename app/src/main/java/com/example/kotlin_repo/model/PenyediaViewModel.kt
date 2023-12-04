8f1c56d279740e87ffb517f24ae91c9367ad6291
Ayash13 <m.ayashal.f@gmail.com>
2023-12-04 21:33:15 +0700

    Create external function query for Applicaation object and return instance from AplikasiSiswa

--- app/src/main/java/com/example/kotlin_repo/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/kotlin_repo/model/PenyediaViewModel.kt
@@ -1,5 +1,15 @@
 package com.example.kotlin_repo.model
 
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.kotlin_repo.AplikasiSiswa
+
 object PenyediaViewModel {
 
 }
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
