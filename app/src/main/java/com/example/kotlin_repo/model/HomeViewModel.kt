73c8c2f5dbb7588f30c1a393889bdbe6cf9ba4d8
Ayash13 <m.ayashal.f@gmail.com>
2023-12-04 21:09:16 +0700

    create data class HomeUiState

--- app/src/main/java/com/example/kotlin_repo/model/HomeViewModel.kt
+++ app/src/main/java/com/example/kotlin_repo/model/HomeViewModel.kt
@@ -1,10 +1,16 @@
 package com.example.kotlin_repo.model
 
 import androidx.lifecycle.ViewModel
+import com.example.kotlin_repo.data.Siswa
 import com.example.kotlin_repo.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.StateFlow
 
-class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
+
 }
