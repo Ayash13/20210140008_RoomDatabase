01a3865f264d79889d75519b48ec69e7ee80844d
Ayash13 <m.ayashal.f@gmail.com>
2023-12-04 21:03:23 +0700

    create companion object to specify suspend time

--- app/src/main/java/com/example/kotlin_repo/model/HomeViewModel.kt
+++ app/src/main/java/com/example/kotlin_repo/model/HomeViewModel.kt
@@ -4,4 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.example.kotlin_repo.repositori.RepositoriSiswa
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
