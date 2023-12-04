c5b64d5a2d75b8ef92c0dec1bad5cc4566f999f9
Ayash13 <m.ayashal.f@gmail.com>
2023-12-04 23:41:01 +0700

    add navigateUp = navigasiBack on SiswaTopAppbar do it can back with arrow button

--- app/src/main/java/com/example/kotlin_repo/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/kotlin_repo/ui/theme/halaman/HalamanEntry.kt
@@ -52,7 +52,8 @@ fun EntrySiswaScreen(
             SiswaTopAppBar(
                 title = stringResource(DestinasiEntry.titleRes),
                 canNavigateBack = true,
-                scrollBehavior = scrollBehavior
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigasiBack
             )
         }
     ) { innerPadding ->
