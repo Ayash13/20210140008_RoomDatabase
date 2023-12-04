bc2d36148575beb17ad2a21cc4e83d6ea615a81c
Ayash13 <m.ayashal.f@gmail.com>
2023-12-04 22:34:33 +0700

    Create HalamanHome then create DestinasiHome Object extend DestinasiNavigas

---
+++ app/src/main/java/com/example/kotlin_repo/ui/theme/halaman/HalamanHome.kt
@@ -0,0 +1,9 @@
+package com.example.kotlin_repo.ui.theme.halaman
+
+import com.example.kotlin_repo.R
+import com.example.kotlin_repo.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route: "home"
+    override val titleRes = R.string.app_name
+}
